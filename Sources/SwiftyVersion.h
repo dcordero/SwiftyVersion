ec260d3ed42195ae229d979edcefa27941017498
David Cordero <dcorderoramirez@gmail.com>
2017-03-30 11:12:53 +0200

    Fix xcodeproj

---
+++ Sources/SwiftyVersion.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyVersion.h
+//  SwiftyVersion
+//
+//  Created by David Cordero on 30.03.17.
+//  Copyright © 2017 David Cordero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyVersion.
+FOUNDATION_EXPORT double SwiftyVersionVersionNumber;
+
+//! Project version string for SwiftyVersion.
+FOUNDATION_EXPORT const unsigned char SwiftyVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyVersion/PublicHeader.h>
+
+
